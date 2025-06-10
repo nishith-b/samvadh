7fa66a4ca9d4a62bf16f62322080d253fcbd42b2
nishithbl <nishithbl2004@gmail.com>
2025-06-10 18:51:45 +0530

    Setup Basic Routing for all routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import LoginForm from "./pages/Auth/LoginForm";
+import SignUpForm from "./pages/Auth/SignUpForm";
+import CreatePoll from "./pages/Dashboard/CreatePoll";
+import MyPolls from "./pages/Dashboard/MyPolls";
+import VotedPolls from "./pages/Dashboard/VotedPolls";
+import Bookmarks from "./pages/Dashboard/Bookmarks";
 
 const App = () => {
   return (
-    <div className='text-prim'>App</div>
-  )
-}
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<LoginForm />} />
+          <Route path="/signup" exact element={<SignUpForm />} />
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="/create-poll" exact element={<CreatePoll />} />
+          <Route path="/my-poll" exact element={<MyPolls />} />
+          <Route path="/voted-poll" exact element={<VotedPolls />} />
+          <Route path="/bookmarked-polls" exact element={<Bookmarks />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
+
+//Define the Root component to handle the intial redirect
+const Root = () => {
+  //Check if token exists in localStorage
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  //Redirect to dashboard if authenticated, otherwise to login
+
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
