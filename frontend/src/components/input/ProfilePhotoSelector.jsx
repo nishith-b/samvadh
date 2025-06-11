8b00e6cc2c8e3954faf66326f006657bf38da3c8
nishith-b <nishith.one@gmail.com>
2025-06-11 21:49:01 +0530

    Setup Signup form with Validation

---
+++ frontend/src/components/input/ProfilePhotoSelector.jsx
@@ -0,0 +1,71 @@
+import React, { useRef, useState } from "react";
+import { LuUser, LuUpload, LuTrash } from "react-icons/lu";
+
+const ProfilePhotoSelector = ({ image, setImage }) => {
+  const inputRef = useRef(null);
+  const [previewUrl, setPreviewUrl] = useState(null);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      //Update the image state
+      setImage(file);
+
+      //Generate Preview URL from the file
+      const preview = URL.createObjectURL(file);
+      setPreviewUrl(preview);
+    }
+  };
+  const handleRemoveImage = () => {
+    setImage(null);
+    setPreviewUrl(null);
+    if (inputRef.current) {
+      inputRef.current.value = null; // reset input value
+    }
+  };
+
+  const onChooseFile = () => {
+    inputRef.current.click();
+  };
+
+  return (
+    <div className="flex justify-center mb-6">
+      <input
+        type="file"
+        accept="image/*"
+        ref={inputRef}
+        onChange={handleImageChange}
+        className="hidden"
+      />
+      {!image ? (
+        <div className="relative flex items-center justify-center w-20 h-20 rounded-full bg-sky-100">
+          <LuUser className="text-4xl text-primary" />
+          <button
+            type="button"
+            className="absolute flex items-center justify-center w-8 h-8 text-white rounded-full bg-primary -bottom-1 -right-1"
+            onClick={onChooseFile}
+          >
+            <LuUpload className="text-base" />
+          </button>
+        </div>
+      ) : (
+        <div className="relative">
+          <img
+            src={previewUrl}
+            alt="profile photo"
+            className="object-cover w-20 h-20 rounded-full"
+          />
+          <button
+            type="button"
+            className="absolute flex items-center justify-center w-8 h-8 text-white bg-red-500 rounded-full -bottom-1 -right-1"
+            onClick={handleRemoveImage}
+          >
+            <LuTrash className="text-base" />
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProfilePhotoSelector;
